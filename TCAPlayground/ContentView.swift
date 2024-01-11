c2eb26bc9d4d83d6fd7414cc38a1687933688674
dazy1030 <odajima_naoki@cyberagent.co.jp>
2024-01-11 14:45:02 +0900

    Initial Commit

---
+++ TCAPlayground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCAPlayground
+//
+//  Created by 小田島 直樹 on 1/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCAPlayground/TCAPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAPlaygroundApp.swift
+//  TCAPlayground
+//
+//  Created by 小田島 直樹 on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TCAPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
